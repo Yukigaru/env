5310cf91c71d0c61bd1bb5f42bb73072215b729c
Alexander Garustovich <yukigaru@gmail.com>
2019-05-22 19:10:25 +0300

    - added ubuntu cleaning script

---
+++ clean-ubuntu-gnome.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+sudo apt remove -y shotwell thunderbird gnome-sudoku libqqwing2v5 rhythmbox rhythmbox-data gir1.2-rb-3.0 librhythmbox-core9 gnome-mines aisleriot gnome-orca libreoffice-impress
