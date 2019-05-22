2ac182e0cb74c5b59bb0559605bdb8cb184433ca
Alexander Garustovich <yukigaru@gmail.com>
2019-05-22 19:11:07 +0300

    - removing .profile before installing, mkdir .config

--- install.sh
+++ install.sh
@@ -4,5 +4,9 @@ ln -s -T ~/env/.vimrc ~/.vimrc
 ln -s -T ~/env/.zshrc ~/.zshrc
 ln -s -T ~/env/.gitconfig ~/.gitconfig
 ln -s -T ~/env/.gdbinit ~/.gdbinit
+
+rm -f ~/.profile
 ln -s -T ~/env/.profile ~/.profile
+
+mkdir -p ~/.config/i3
 ln -s -T ~/env/.config/i3/config ~/.config/i3/config
