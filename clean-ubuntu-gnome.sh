da20dfed952c25cc4f52fe20fef55e1ced496a95
Alexander Garustovich <yukigaru@gmail.com>
2019-05-25 11:44:04 +0300

    - corrected clean script

--- clean-ubuntu-gnome.sh
+++ clean-ubuntu-gnome.sh
@@ -1,2 +1,13 @@
 #!/usr/bin/env bash
-sudo apt remove -y shotwell thunderbird gnome-sudoku libqqwing2v5 rhythmbox rhythmbox-data gir1.2-rb-3.0 librhythmbox-core9 gnome-mines aisleriot gnome-orca libreoffice-impress
+sudo apt remove -y shotwell
+sudo apt remove -y thunderbird
+sudo apt remove -y gnome-sudoku
+sudo apt remove -y libqqwing2v5
+sudo apt remove -y rhythmbox
+sudo apt remove -y rhythmbox-data
+sudo apt remove -y gir1.2-rb-3.0
+sudo apt remove -y librhythmbox-core9
+sudo apt remove -y gnome-mines
+sudo apt remove -y aisleriot
+sudo apt remove -y gnome-orca
+sudo apt remove -y libreoffice-impress
