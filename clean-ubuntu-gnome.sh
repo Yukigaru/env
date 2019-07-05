2d56a133240675180504e596a52ec5ed7b7b010c
Alexander Garustovich <yukigaru@gmail.com>
2019-07-05 09:56:52 +0300

    - removing of the unused ubuntu home folders

--- clean-ubuntu-gnome.sh
+++ clean-ubuntu-gnome.sh
@@ -11,3 +11,10 @@ sudo apt remove -y gnome-mines
 sudo apt remove -y aisleriot
 sudo apt remove -y gnome-orca
 sudo apt remove -y libreoffice-impress
+sudo apt remove -y shotwell thunderbird gnome-sudoku libqqwing2v5 rhythmbox rhythmbox-data gir1.2-rb-3.0 librhythmbox-core9 gnome-mines aisleriot gnome-orca libreoffice-impress
+rm -rf ~/Documents
+rm -rf ~/Downloads
+rm -rf ~/Music
+rm -rf ~/Pictures
+rm -rf ~/Templates
+rm -rf ~/Videos
