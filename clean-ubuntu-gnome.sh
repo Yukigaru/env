1d4c43ad2e214eb851fd1395e7527b2736ca8445
Alexander Garustovich <yukigaru@gmail.com>
2019-08-07 19:04:35 +0300

    - updated clean script

--- clean-ubuntu-gnome.sh
+++ clean-ubuntu-gnome.sh
@@ -1,19 +1,17 @@
 #!/usr/bin/env bash
-sudo apt remove -y shotwell
-sudo apt remove -y thunderbird
-sudo apt remove -y gnome-sudoku
-sudo apt remove -y libqqwing2v5
-sudo apt remove -y rhythmbox
-sudo apt remove -y rhythmbox-data
-sudo apt remove -y gir1.2-rb-3.0
-sudo apt remove -y librhythmbox-core9
-sudo apt remove -y gnome-mines
-sudo apt remove -y aisleriot
-sudo apt remove -y gnome-orca
-sudo apt remove -y libreoffice-impress
-sudo apt remove -y shotwell thunderbird gnome-sudoku libqqwing2v5 rhythmbox rhythmbox-data gir1.2-rb-3.0 librhythmbox-core9 gnome-mines aisleriot gnome-orca libreoffice-impress
+sudo apt remove -y shotwell || true
+sudo apt remove -y thunderbird || true
+sudo apt remove -y gnome-sudoku || true
+sudo apt remove -y libqqwing2v5 || true
+sudo apt remove -y rhythmbox || true
+sudo apt remove -y rhythmbox-data || true
+sudo apt remove -y gir1.2-rb-3.0 || true
+sudo apt remove -y librhythmbox-core9 || true
+sudo apt remove -y gnome-mines || true
+sudo apt remove -y aisleriot || true
+sudo apt remove -y gnome-orca || true
+sudo apt remove -y libreoffice-impress || true
 rm -rf ~/Documents
-rm -rf ~/Downloads
 rm -rf ~/Music
 rm -rf ~/Pictures
 rm -rf ~/Templates
