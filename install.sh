7562629ca0c7467fae35118cc04fbeb769f8804b
Alexander Garustovich <yukigaru@gmail.com>
2020-01-26 19:40:04 +0300

    - added mc config

--- install.sh
+++ install.sh
@@ -26,6 +26,9 @@ ln -s -T ~/env/.config/i3/config ~/.config/i3/config
 mkdir -p ~/.config/nvim
 ln -s -T ~/env/.config/nvim/init.vim ~/.config/nvim/init.vim
 
+mkdir -p ~/.config/mc
+ln -s -T ~/env/.config/mc/mc.keymap ~/.config/mc/mc.keymap
+
 mkdir -p ~/.config/sublime-text-3/Packages/User
 ln -s -T ~/env/Preferences.sublime-settings ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
 
