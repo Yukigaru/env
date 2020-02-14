f3598168c07b851c195f253f0bd7538dddd39738
Alexander Garustovich <yukigaru@gmail.com>
2020-02-14 14:34:29 +0300

    Added more items to clean-ubuntu-gnome

--- clean-ubuntu-gnome.sh
+++ clean-ubuntu-gnome.sh
@@ -2,12 +2,13 @@
 sudo apt remove -y shotwell || true
 sudo apt remove -y thunderbird || true
 sudo apt remove -y gnome-sudoku || true
+sudo apt remove -y gnome-mines || true
+sudo apt remove -y gnome-mahjongg || true
 sudo apt remove -y libqqwing2v5 || true
 sudo apt remove -y rhythmbox || true
 sudo apt remove -y rhythmbox-data || true
 sudo apt remove -y gir1.2-rb-3.0 || true
 sudo apt remove -y librhythmbox-core9 || true
-sudo apt remove -y gnome-mines || true
 sudo apt remove -y aisleriot || true
 sudo apt remove -y gnome-orca || true
 sudo apt remove -y libreoffice-impress || true
