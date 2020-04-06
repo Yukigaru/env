322cf82f5cc9c24247e79246b9f606c8d836752d
Alexander Garustovich <yukigaru@gmail.com>
2020-04-06 18:17:16 +0300

    Removed clang installation

--- apps.sh
+++ apps.sh
@@ -42,21 +42,5 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -s -T ~/env/.config/nvim/init.vim ~/.config/init.vim
 nvim +PluginInstall +qall
 
-# install clang-7
-wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
-SOURCE_LIST=/etc/apt/sources.list.d/clang.list
-if [ $RELEASE = "16.04" ]; then
-    echo "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-8 main" >$SOURCE_LIST
-    echo "deb-src http://apt.llvm.org/xenial/ llvm-toolchain-xenial-8 main" >>$SOURCE_LIST
-elif [ $RELEASE = "18.04" ]; then
-    echo "deb http://apt.llvm.org/cosmic/ llvm-toolchain-bionic-8 main" >$SOURCE_LIST
-    echo "deb-src http://apt.llvm.org/cosmic/ llvm-toolchain-bionic-8 main" >>$SOURCE_LIST
-elif [ $RELEASE = "19.04" ]; then
-    echo "deb http://apt.llvm.org/cosmic/ llvm-toolchain-disco-8 main" >$SOURCE_LIST
-    echo "deb-src http://apt.llvm.org/cosmic/ llvm-toolchain-disco-8 main" >>$SOURCE_LIST
-fi
-apt update
-apt install -y clang-7 lldb-7 lld-7 libfuzzer-7-dev libc++-7-dev libc++abi-7-dev
-
 
 
