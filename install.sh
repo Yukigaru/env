3a40e15758259832d0cc36fc66a96b6a7cfaa264
Alexander Garustovich <yukigaru@gmail.com>
2022-01-28 17:31:31 +0300

    Rework install.sh script: less error output

--- install.sh
+++ install.sh
@@ -1,37 +1,34 @@
 #!/usr/bin/env bash
-mkdir -p backups
+BACKUP_DIR="./backup `date +"%Y-%m-%d %H-%M-%S"`"
+mkdir -p "${BACKUP_DIR}"
 touch ~/.localvars
 
-mv ~/.bash_aliases ./backups/ || true
-mv ~/.zshrc ./backups/ || true
-mv ~/.gitconfig ./backups/ || true
-mv ~/.profile ./backups/ || true
-mv ~/.gdbinit ./backups/ || true
-mv ~/.tmux.confg ./backups/ || true
-mv ~/.config/i3/config ./backups/ || true
-mv ~/.config/nvim/init.vim ./backups/ || true
-mv ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings ./backups/ || true
-mv ~/.imwheelrc ./backups/ || true
+mv ~/.bash_aliases ${BACKUP_DIR} 2>/dev/null || true
+ln -f -s -T ~/env/.bash_aliases ~/.bash_aliases
 
-ln -s -T ~/env/.bash_aliases ~/.bash_aliases
-ln -s -T ~/env/.zshrc ~/.zshrc
-ln -s -T ~/env/.gitconfig ~/.gitconfig
-ln -s -T ~/env/.gdbinit ~/.gdbinit
-ln -s -T ~/env/.tmux.conf ~/.tmux.conf
+mv ~/.zshrc ${BACKUP_DIR} 2>/dev/null || true
+ln -f -s -T ~/env/.zshrc ~/.zshrc
 
-rm -f ~/.profile
-ln -s -T ~/env/.profile ~/.profile
+mv ~/.gitconfig ${BACKUP_DIR} 2>/dev/null || true
+ln -f -s -T ~/env/.gitconfig ~/.gitconfig
+
+mv ~/.gdbinit ${BACKUP_DIR} 2>/dev/null || true
+ln -f -s -T ~/env/.gdbinit ~/.gdbinit
+
+mv ~/.tmux.confg ${BACKUP_DIR} 2>/dev/null || true
+ln -f -s -T ~/env/.tmux.conf ~/.tmux.conf
+
+mv ~/.profile ${BACKUP_DIR} 2>/dev/null || true
+ln -f -s -T ~/env/.profile ~/.profile
 
 mkdir -p ~/.config/i3
-ln -s -T ~/env/.config/i3/config ~/.config/i3/config
+mv ~/.config/i3/config ${BACKUP_DIR} 2>/dev/null || true
+ln -f -s -T ~/env/.config/i3/config ~/.config/i3/config
 
 mkdir -p ~/.config/nvim
-ln -s -T ~/env/.config/nvim/init.vim ~/.config/nvim/init.vim
-
-mkdir -p ~/.config/mc
-ln -s -T ~/env/.config/mc/mc.keymap ~/.config/mc/mc.keymap
+mv ~/.config/nvim/init.vim ${BACKUP_DIR} 2>/dev/null || true
+ln -f -s -T ~/env/.config/nvim/init.vim ~/.config/nvim/init.vim
 
 mkdir -p ~/.config/sublime-text-3/Packages/User
-ln -s -T ~/env/Preferences.sublime-settings ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
-
-ln -s -T ~/env/.imwheelrc ~/.imwheelrc
+mv ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings ${BACKUP_DIR} 2>/dev/null || true
+ln -f -s -T ~/env/Preferences.sublime-settings ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
