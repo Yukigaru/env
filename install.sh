1e32980a79aa8c22d5e7f6fe34ca10631ffd3add
Alexander Garustovich <yukigaru@gmail.com>
2026-03-04 15:24:16 +0300

    install: drop GNU-only -T flag, fix tmux.conf typo

--- install.sh
+++ install.sh
@@ -4,39 +4,42 @@ mkdir -p "${BACKUP_DIR}"
 touch ~/.localvars
 
 mv ~/.bash_aliases ${BACKUP_DIR} 2>/dev/null || true
-ln -f -s -T ~/env/.bash_aliases ~/.bash_aliases
+ln -f -s ~/env/.bash_aliases ~/.bash_aliases
 
 mv ~/.zshrc ${BACKUP_DIR} 2>/dev/null || true
-ln -f -s -T ~/env/.zshrc ~/.zshrc
+ln -f -s ~/env/.zshrc ~/.zshrc
 
 mv ~/.gitconfig ${BACKUP_DIR} 2>/dev/null || true
-ln -f -s -T ~/env/.gitconfig ~/.gitconfig
+ln -f -s ~/env/.gitconfig ~/.gitconfig
+
+mv ~/.gitconfig-work ${BACKUP_DIR} 2>/dev/null || true
+ln -f -s ~/env/.gitconfig-work ~/.gitconfig-work
 
 mv ~/.gdbinit ${BACKUP_DIR} 2>/dev/null || true
-ln -f -s -T ~/env/.gdbinit ~/.gdbinit
+ln -f -s ~/env/.gdbinit ~/.gdbinit
 
-mv ~/.tmux.confg ${BACKUP_DIR} 2>/dev/null || true
-ln -f -s -T ~/env/.tmux.conf ~/.tmux.conf
+mv ~/.tmux.conf ${BACKUP_DIR} 2>/dev/null || true
+ln -f -s ~/env/.tmux.conf ~/.tmux.conf
 
 mv ~/.profile ${BACKUP_DIR} 2>/dev/null || true
-ln -f -s -T ~/env/.profile ~/.profile
+ln -f -s ~/env/.profile ~/.profile
 
 mkdir -p ~/.config/htop
 mv ~/.config/htop/htoprc ${BACKUP_DIR} 2>/dev/null || true
-ln -f -s -T ~/env/.config/htoprc ~/.config/htop/htoprc
+ln -f -s ~/env/.config/htoprc ~/.config/htop/htoprc
 
 mkdir -p ~/.config/i3
 mv ~/.config/i3/config ${BACKUP_DIR} 2>/dev/null || true
-ln -f -s -T ~/env/.config/i3/config ~/.config/i3/config
+ln -f -s ~/env/.config/i3/config ~/.config/i3/config
 
 mkdir -p ~/.config/nvim
 mv ~/.config/nvim/init.vim ${BACKUP_DIR} 2>/dev/null || true
-ln -f -s -T ~/env/.config/nvim/init.vim ~/.config/nvim/init.vim
+ln -f -s ~/env/.config/nvim/init.vim ~/.config/nvim/init.vim
 
 mkdir -p ~/.config/sublime-text-3/Packages/User
 mv ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings ${BACKUP_DIR} 2>/dev/null || true
-ln -f -s -T ~/env/Preferences.sublime-settings ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
+ln -f -s ~/env/Preferences.sublime-settings ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
 
 mkdir -p ~/.config/ranger
 mv ~/.config/i3/config ${BACKUP_DIR} 2>/dev/null || true
-ln -f -s -T ~/env/.config/ranger/rc.conf ~/.config/ranger/rc.conf
+ln -f -s ~/env/.config/ranger/rc.conf ~/.config/ranger/rc.conf
